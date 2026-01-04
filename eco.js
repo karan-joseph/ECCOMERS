54e2a0718aa09ef30d1072938638bf4fcd5739b2
karan <karanjarmy@gmail.com>
2026-01-04 12:46:05 +0530

    ecommerce

---
+++ eco.js
@@ -0,0 +1,10 @@
+// selecting side_navbar
+var sidenavbar = document.querySelector(".side_navbar");
+
+function shownavbar() {
+    sidenavbar.style.left = "0";
+}
+
+function closenavbar() {
+    sidenavbar.style.left = "-50%";
+}

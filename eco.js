22a1b3de235604a96c6f31fd32650756ee53e1f3
karan <karanjarmy@gmail.com>
2026-01-13 07:19:05 +0530

    search option

--- eco.js
+++ eco.js
@@ -8,3 +8,15 @@ function shownavbar() {
 function closenavbar() {
     sidenavbar.style.left = "-50%";
 }
+
+// selecting search box
+var searchbox = document.querySelector(".navbar-search");
+var header = document.querySelector(".header");
+var headercontainer = document.querySelector(".header-container");
+
+searchbox.addEventListener("keyup", function () {
+    var searchvalue = event.target.value.toUpperCase();
+
+    for(count<0;count<headercontainer.leangth;count++){
+        var productname = 
+});
